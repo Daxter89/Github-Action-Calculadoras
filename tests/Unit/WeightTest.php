4f204720aeee100c797aa3cd394ad1492c2f4d84
Xavi Campos <daxterxavi@gmail.com>
2024-04-11 21:14:29 +0200

    Cambio de los nombres de los test unitarios.

--- tests/Unit/WeightTest.php
+++ tests/Unit/WeightTest.php
@@ -3,11 +3,10 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use App\Models\PesoIdeal;
 use App\Models\idealWeight;
 
 use App\Http\Controllers\IdealWeightController;
-class TestIdealWeightController extends TestCase
+class WeightTest extends TestCase
 {
     /**
      * A basic unit test example.
@@ -17,11 +16,11 @@ class TestIdealWeightController extends TestCase
         $height = 180; // parámetros
         $gender = 'female';
 
-        $calculaPesoIdeal = new  IdealWeight(); //creo el objeto 
+        $calculaPesoIdeal = new  IdealWeight(); //creo el objeto
         $resultado = $calculaPesoIdeal->caculaPesoIdeal($height, $gender);
         $esperado = 45 + 2.3 * ($height -60);// El resultado que se espera
 
         // Verifico que el resultado del cálculo sea el esperado
-        $this->assertEquals($esperado, $resultado);       
+        $this->assertEquals($esperado, $resultado);
     }
 }
